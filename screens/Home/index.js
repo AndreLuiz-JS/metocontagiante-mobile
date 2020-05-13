6f39ef6ec58c63517aef7bf1f6b772462b6c90c7
AndreLuiz-JS <oxxxandreluizxxxo@gmail.com>
2020-05-13 16:06:13 -0300

    Bloqueado acesso off-line às telas que consomem dados na rede

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { ImageBackground, Image, Text, TouchableOpacity, View, Linking, FlatList } from 'react-native';
+import { ImageBackground, Image, Text, TouchableOpacity, View, Linking, FlatList, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useNetInfo } from '@react-native-community/netinfo';
 
 import { Feather, FontAwesome5 } from '@expo/vector-icons';
 import { styles } from './styles';
@@ -12,77 +13,90 @@ import logo from '../../assets/images/icon.png';
 
 export default function HomeScreen() {
   const navigation = useNavigation();
+  const netInfo = useNetInfo();
   const icons = [ {
     route: 'Bible',
     iconName: 'book',
     iconType: 'Feather',
-    name: 'Bíblia'
+    name: 'Bíblia',
+    onLine: false
   },
   {
     route: 'Cell',
     iconName: 'users',
     iconType: 'Feather',
-    name: 'Células'
+    name: 'Células',
+    onLine: true,
   },
   {
     route: 'Photo',
     iconName: 'camera',
     iconType: 'Feather',
-    name: 'Fotos'
+    name: 'Fotos',
+    onLine: true
   },
   {
     route: 'Schedule',
     iconName: 'calendar',
     iconType: 'Feather',
-    name: 'Agenda'
+    name: 'Agenda',
+    onLine: true
   },
   {
     route: 'Devotional',
     iconName: 'pray',
     iconType: 'FontAwesome5',
-    name: 'Devocional'
+    name: 'Devocional',
+    onLine: true
   },
   {
     route: 'Home',
     iconName: 'users',
     iconType: 'FontAwesome5',
-    name: 'Pastores'
+    name: 'Pastores',
+    onLine: false
   },
   {
     route: 'Podcast',
     iconName: 'podcast',
     iconType: 'FontAwesome5',
-    name: 'Podcast'
+    name: 'Podcast',
+    onLine: true
   },
   {
     route: 'SocialMedia',
     iconName: 'thumbs-up',
     iconType: 'FontAwesome5',
-    name: 'Mídias Sociais'
+    name: 'Mídias Sociais',
+    onLine: false
   },
   {
     openUrl: 'whatsapp://send?phone=5522992797679',
     iconName: 'whatsapp',
     iconType: 'FontAwesome5',
-    name: 'Contato'
+    name: 'Contato',
+    onLine: false
   },
   {
     route: 'PrayOrder',
     iconName: 'file-signature',
     iconType: 'FontAwesome5',
-    name: 'Pedido de Oração'
+    name: 'Pedido de Oração',
+    onLine: true
   },
   {
     route: 'Home',
     iconName: 'id-card',
     iconType: 'FontAwesome5',
-    name: 'Quem Somos'
+    name: 'Quem Somos',
+    onLine: false
   },
   {
     route: 'Map',
     iconName: 'map-pin',
     iconType: 'Feather',
-    name: 'Como Chegar'
+    name: 'Como Chegar',
+    onLine: true
   }, ]
   return (
     <ImageBackground source={background} style={styles.container}>
@@ -96,7 +110,7 @@ export default function HomeScreen() {
         renderItem={({ item, index }) => (
           <TouchableOpacity
             style={styles.ico}
-            onPress={item.route ? () => navigation.navigate(item.route) : () => Linking.openURL(item.openUrl)}
+            onPress={() => handlePressPageIcon(item)}
           >
             {item.iconType === 'Feather' && (<Feather name={item.iconName} size={40} color={colors.primary} />)}
             {item.iconType === 'FontAwesome5' && (<FontAwesome5 name={item.iconName} size={40} color={colors.primary} />)}
@@ -108,5 +122,14 @@ export default function HomeScreen() {
 
     </ImageBackground >
   );
+
+  function handlePressPageIcon(item) {
+    if (!netInfo.isConnected && item.onLine) {
+      Alert.alert('Sem conexão com a internet', 'Verifique sua conexão com a internet, essa funcionalidade não está disponível off-line.');
+    } else {
+      if (item.route) navigation.navigate(item.route);
+      if (item.openUrl) Linking.openURL(item.openUrl);
+    }
+  }
 }
 
